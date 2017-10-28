57598d244aae4df142e6f36d9bb8246c664809e1
Stefan Larsson <stefan@larsson.nu>
2017-10-28 22:43:35 +0200

    Added more code from course materials.
    Got decent training accuracy in test-method.

--- vehicle_detection.py
+++ vehicle_detection.py
@@ -5,11 +5,21 @@ from moviepy.editor import VideoFileClip
 from functools import partial
 import os
 import glob
+import time
+from skimage.feature import hog
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
 from skimage.feature import hog
 
 orient = 9
 pix_per_cell = 8
 cell_per_block = 2
+
+
+
 
 
 # From "34. Search and Classify"
@@ -332,12 +342,72 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, ce
 # From "40. Tips and Tricks for the Project"
 # TODO where to use this method from?
 def hog_feature_array(orient, pix_per_cell, cell_per_block):
-    hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell), cells_per_block=(cell_per_block, cell_per_block), visualise=False, feature_vector=False)
+    hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+        cells_per_block=(cell_per_block, cell_per_block), visualise=False, feature_vector=False)
+
+
+# From "29. HOG Classify" with modifications
+# TODO this method just exercises the code up until now, adapt for final assignment
+def predict_cars():
+    # TODO: Tweak these parameters and see how the results change.
+    colorspace = 'RGB'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+    orient = 9
+    pix_per_cell = 8
+    cell_per_block = 2
+    hog_channel = 0  # Can be 0, 1, 2, or "ALL"
+
+    cars = glob.glob('vehicles/GTI*/*.png')
+    notcars = glob.glob('non-vehicles/**/*.png')
+    t = time.time()
+    car_features = extract_features(cars, color_space=colorspace, orient=orient,
+                                    pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                                    hog_channel=hog_channel)
+    notcar_features = extract_features(notcars, color_space=colorspace, orient=orient,
+                                       pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                                       hog_channel=hog_channel)
+
+    t2 = time.time()
+    print(round(t2 - t, 2), 'Seconds to extract HOG features...')
+    # Create an array stack of feature vectors
+    X = np.vstack((car_features, notcar_features)).astype(np.float64)
+    # Fit a per-column scaler
+    X_scaler = StandardScaler().fit(X)
+    # Apply the scaler to X
+    scaled_X = X_scaler.transform(X)
+
+    # Define the labels vector
+    y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+    # Split up data into randomized training and test sets
+    rand_state = np.random.randint(0, 100)
+    X_train, X_test, y_train, y_test = train_test_split(
+        scaled_X, y, test_size=0.2, random_state=rand_state)
+
+    print('Using:', orient, 'orientations', pix_per_cell,
+          'pixels per cell and', cell_per_block, 'cells per block')
+    print('Feature vector length:', len(X_train[0]))
+    # TODO if tuning needed, see "27. Parameter tuning"
+    # Use a linear SVC
+    svc = LinearSVC()
+    # Check the training time for the SVC
+    t = time.time()
+    svc.fit(X_train, y_train)
+    t2 = time.time()
+    print(round(t2 - t, 2), 'Seconds to train SVC...')
+    # Check the score of the SVC
+    print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+    # Check the prediction time for a single sample
+    t = time.time()
+    n_predict = 10
+    print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+    print('For these', n_predict, 'labels: ', y_test[0:n_predict])
+    t2 = time.time()
+    print(round(t2 - t, 5), 'Seconds to predict', n_predict, 'labels with SVC')
 
 
 # Main, process the videos
 def vehicle_detection_main():
-    # TODO Go to "29. HOG Classify", copy-paste line 70-end here to test SVM training
+    predict_cars()
 
     # Find cars with "35. Hog Sub-sampling Window Search" code
     img = mpimg.imread('test_images/test1.jpg')
